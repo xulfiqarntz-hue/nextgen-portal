33dd3887c07632f7de0aca0d29fb83a1378dc86a
Zulfiqar Ali <zulfiqar@nextgeneacademy.com>
2026-07-05 19:40:49 +0500

    Login page working end to end

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('node:dns/promises').setServers(['1.1.1.1', '8.8.8.8']);
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const authRoutes = require('./routes/auth');
 const assignRoutes = require('./routes/assign');
 const chatRoutes = require('./routes/chat');
@@ -16,13 +17,11 @@ mongoose.connect(dbURI)
   .catch((err) => console.log('MongoDB connection error:', err));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/auth', authRoutes);
 app.use('/api', assignRoutes);
 app.use('/api/chat', chatRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello Zulfiqar! Your server is working.');
-});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
